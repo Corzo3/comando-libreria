3f230ac873e71c616985347ca9d4590f59ad9bab
IGuijarroJ <iguijarro3@gmail.com>
2022-04-28 11:45:39 +0200

    Modificando clases

--- src/main/java/es/mde/comando/Maleta.java
+++ src/main/java/es/mde/comando/Maleta.java
@@ -3,32 +3,38 @@ package es.mde.comando;
 import java.util.Collection;
 
 public class Maleta {
+  private Long id;
+  private Collection<ElementoEquipo> elementos;
+  private Double pesoEnVacio;
 
-	Collection<ElementoEquipo> elementos;
-	Double pesoEnVacio;
+  public Collection<ElementoEquipo> getElementos() {
+    return elementos;
+  }
 
-	public Collection<ElementoEquipo> getElementos() {
-		return elementos;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public Double getPesoEnVacio() {
-		return pesoEnVacio;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public void setElementos(Collection<ElementoEquipo> elementos) {
-		this.elementos = elementos;
-	}
+  public Double getPesoEnVacio() {
+    return pesoEnVacio;
+  }
 
-	public void setPesoEnVacio(Double pesoEnVacio) {
-		this.pesoEnVacio = pesoEnVacio;
-	}
+  public void setElementos(Collection<ElementoEquipo> elementos) {
+    this.elementos = elementos;
+  }
 
-	public Maleta(Double pesoEnVacio) {
-		this.pesoEnVacio = pesoEnVacio;
-	}
-	
-	
-	
-	
+  public void setPesoEnVacio(Double pesoEnVacio) {
+    this.pesoEnVacio = pesoEnVacio;
+  }
+
+  public Maleta(Double pesoEnVacio) {
+    this.pesoEnVacio = pesoEnVacio;
+  }
+
+
 
 }
