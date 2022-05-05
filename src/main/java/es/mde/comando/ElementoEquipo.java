16b8b157e265a5e6e1b546e0bbfa24d406c7da58
IGuijarroJ <iguijarro3@gmail.com>
2022-05-05 18:58:57 +0200

    Creando string id para pruebas

--- src/main/java/es/mde/comando/ElementoEquipo.java
+++ src/main/java/es/mde/comando/ElementoEquipo.java
@@ -2,20 +2,14 @@ package es.mde.comando;
 
 public class ElementoEquipo {
 
-	private String nombre;
+	
 	private Double peso;
 
-	public String getNombre() {
-		return nombre;
-	}
 
 	public double getPeso() {
 		return peso;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
 
 	public void setPeso(double peso) {
 		this.peso = peso;
@@ -24,9 +18,9 @@ public class ElementoEquipo {
 	public ElementoEquipo() {
 	}
 
-	public ElementoEquipo(String nombre, Double peso) {
-		this.nombre = nombre;
+
+	public ElementoEquipo(Double peso) {
+		super();
 		this.peso = peso;
 	}
-
 }
